80872a0a92ed986252bedd2bfb9df0cc72fec677
Sebastian Diaz Avila <sebastiandiazavila@Sebastians-MacBook-Pro.local>
2019-11-14 18:54:06 -0300

    Add provider to the view

---
+++ MVP/Provider/MovieProvider.swift
@@ -0,0 +1,48 @@
+//
+//  MovieProvider.swift
+//  MVP
+//
+//  Created by Sebastian Diaz Avila on 14-11-19.
+//  Copyright © 2019 ITAU. All rights reserved.
+//
+
+import Foundation
+
+protocol ProviderProtocol {
+
+    typealias listMoviesResult = (Result<[Movie],ErrorService>) -> Void
+    typealias movieResult = (Result<Movie,ErrorService>) -> Void
+
+    func getMovie(id: Int, completition: @escaping movieResult )
+    func getTopRated(completition: @escaping listMoviesResult)
+    func getUpcoming(completition: @escaping listMoviesResult)
+    func getPopular(completition: @escaping listMoviesResult)
+}
+
+
+class MovieProvider: ProviderProtocol {
+    
+    var service: ServiceProtocol
+    
+    
+    init(service: ServiceProtocol) {
+        self.service = service
+    }
+    
+    func getMovie(id: Int, completition: @escaping movieResult) {
+        service.getMovie(id: id) { completition($0) }
+    }
+    
+    func getTopRated(completition: @escaping listMoviesResult) {
+        service.getTopRated { completition($0) }
+    }
+    
+    func getUpcoming(completition: @escaping listMoviesResult) {
+        service.getUpcoming { completition($0) }
+    }
+    
+    func getPopular(completition: @escaping listMoviesResult) {
+        service.getPopular { completition($0) }
+    }
+    
+}
